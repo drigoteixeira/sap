8ef914dab813e92d49f65051b19d6843fcc1c138
Rodrigo Teixeira <rodrigo@a2comunicacao.com.br>
2014-01-31 17:53:12 -0200

    Ajuste na formatação do link no rodapé

--- in/footer.php
+++ in/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer" role="contentinfo">
 	<div class="row signature">
 		<div class="twelvecol">
-			<p>&copy; <?php echo date("Y"); ?> Santrode, Todos os direitos reservados. - Foto: <a href="http://www.flickr.com/photos/instantvantage/8363737128/sizes/l/">Base</a> por <a href="http://www.flickr.com/photos/instantvantage/">Instant Vantage</a> - <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC BY-SA 2.0</a></p>
+			<p>&copy; <?php echo date("Y"); ?> Santrode, Todos os direitos reservados. - Foto: <a href="http://www.flickr.com/photos/instantvantage/8363737128/sizes/l/">Even Wachten</a> por <a href="http://www.flickr.com/photos/waagsociety/">by Waag Society</a> - <a href="http://creativecommons.org/licenses/by/2.0/">CC BY 2.0</a></p>
 		</div>
 	</div>
 </footer>
