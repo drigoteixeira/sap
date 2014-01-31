41d65cc48017d5f7d352248832a7cf6f3f095b2c
Rodrigo Teixeira <rodrigo@a2comunicacao.com.br>
2014-01-31 17:26:04 -0200

    Inclusão do crédito da foto no rodapé

--- in/footer.php
+++ in/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer" role="contentinfo">
 	<div class="row signature">
 		<div class="twelvecol">
-			<p>&copy; <?php echo date("Y"); ?> Santrode, Todos os direitos reservados.</p>
+			<p>&copy; <?php echo date("Y"); ?> Santrode, Todos os direitos reservados. - Foto: <a href="http://www.flickr.com/photos/instantvantage/8363737128/sizes/l/">Base</a> por <a href="http://www.flickr.com/photos/instantvantage/">Instant Vantage</a> - <a href="http://creativecommons.org/licenses/by-sa/2.0/"></a>CC BY-SA 2.0</a></p>
 		</div>
 	</div>
 </footer>
