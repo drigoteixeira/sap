f3e07e1b5037d79c80564f6b7cc118d6adc98a82
Rodrigo Teixeira <drigoteixeira@gmail.com>
2014-09-26 15:47:17 -0300

    Reorganização dos templates e paginas de internas

--- js/assets/scripts.js
+++ js/assets/scripts.js
@@ -13,10 +13,6 @@ $menulink.click(function() {
 	return false;
 });
 
-$("nav[role=navigation] li a").click(function() {
-    $menu.toggleClass('active');
-    return false;
-});
 
 // FADE IN TAGLINE
 
@@ -25,44 +21,7 @@ $(".tagline p").delay(1000).animate({opacity: 1}, 700);
 $(".tagline a").delay(1000).animate({opacity: 1}, 1200);
 
 
-// EFEITO MENU ATIVO
 
-$(document).on("scroll", onScroll);
-
-//smoothscroll
-$('a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-    $(document).off("scroll");
-
-    $('a').each(function () {
-        $(this).removeClass('active');
-    });
-    $(this).addClass('active');
-
-    var target = this.hash, menu = target;
-    $target = $(target);
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-    }, 500, 'swing', function () {
-        window.location.hash = target;
-        $(document).on("scroll", onScroll);
-    });
-
-});
-
-function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('#menu a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-    });
-}
 
 
 /* FADE IN NOS ELEMENTOS NO SCROLL DA PÁGINA */
